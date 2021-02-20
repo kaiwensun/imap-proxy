3288a2c1740682b716a42b976d1a7aa8d60d94cb
Kaiwen Sun <skw_kevin@126.com>
2021-02-20 02:24:24 -0800

    hide deactivated token

--- src/local_service/config/local_settings.py
+++ src/local_service/config/local_settings.py
@@ -1,3 +1,3 @@
 USER_NAME = "skw_kevin@126.com"
-IMAP_TOKEN = "ITNJSRRIVWZDHCWM"  # applies to SMTP as well
+IMAP_TOKEN = "this token has be deactivated. no need to attack again"  # applies to SMTP as well
 IMAP_ADDRESS = "imap.126.com"
