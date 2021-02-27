d52ff593a5ccf37c23cbdc0c8977ddedce0b9659
Kaiwen Sun <skw_kevin@126.com>
2021-02-26 21:59:41 -0800

    misc improvement

--- src/chrome_extension/service-worker.js
+++ src/chrome_extension/service-worker.js
@@ -3,21 +3,34 @@
 importScripts('client.js');
 importScripts('logger.js');
 
-const PERIOD_IN_MINUTES = 5;
+const PERIOD_IN_MINUTES = 5;  // must be integer
 const LAST_CHECK_TIME_KEY = "lastCheckTime";
 const CHECK_RESULT_KEY = "checkResult";
 
 function sinceLastCheck(callback) {
     chrome.storage.local.get([LAST_CHECK_TIME_KEY], ({ lastCheckTime }) => {
-        if (lastCheckTime === undefined) {
-            lastCheckTime = 0;
+        lastCheckTime = new Date(lastCheckTime);
+        if (isNaN(lastCheckTime.getTime())) {
+            console.warn(`lastCheckTime is invalid`);
+            lastCheckTime = new Date(0);
         }
-        let now = Date();
-        let remainingMinutes = 0;
-        if (now - (PERIOD_IN_MINUTES * 60 - 10) * 1000 < lastCheckTime) {
-            // if scueduled in less 10 seconds, let's do it now!
-            remainingMinutes = Math.max(0, (now - lastCheckTime) / 1000 / 60 - PERIOD_IN_MINUTES);
-        }
+        console.log(`lastCheckTime is ${lastCheckTime}`);
+
+        let nextCheckTime = new Date(lastCheckTime);
+        nextCheckTime.setSeconds(nextCheckTime.getSeconds() + PERIOD_IN_MINUTES * 60);
+        console.log(`nextCheckTime is ${nextCheckTime}`);
+
+        let loosedNextCheckTime = new Date(nextCheckTime);
+        loosedNextCheckTime.setSeconds(loosedNextCheckTime.getSeconds() - 10);
+        console.log(`nextCheckTime is ${nextCheckTime}`);
+
+        let now = new Date();
+        console.log(`now is ${now}`);
+
+        let remainingMinutes = (nextCheckTime - now) / 1000 / 60;
+        // if timer is going to fire in less than 10 seconds, let's do it now.
+        remainingMinutes = Math.max(remainingMinutes, 10 / 60) - 10 / 60;
+        console.log(`remainingMinutes is ${remainingMinutes}`);
         callback(remainingMinutes);
     })
 }
@@ -32,33 +45,37 @@ let alarmHandlers = {
         sinceLastCheck((remainingMinutes) => {
             if (remainingMinutes === 0) {
                 setLastCheck(new Date().getTime() /* supress other triggers during the check call */, () => {
-                    console.log("checking email");
-                    logger.log("checking email");
-                    client.getUnreadEmailIds(
-                        checkResult => {
-                            setLastCheck(new Date().getTime(), () => {
-                                chrome.storage.local.set({ checkResult }, () => {
-                                    console.log(checkResult);
-                                    logger.log(checkResult);
-                                    chrome.action.setBadgeText({ text: checkResult.count == 0 ? "" : checkResult.count.toString() });
+                    chrome.action.setBadgeBackgroundColor({ color: "#FFA500" }, () => {
+                        console.log("checking email");
+                        logger.log("checking email");
+                        client.getUnreadEmailIds(
+                            checkResult => {
+                                chrome.action.setBadgeBackgroundColor({ color: "#FF0000" }, () => {
+                                    chrome.storage.local.set({ checkResult }, () => {
+                                        console.log(checkResult);
+                                        logger.log(checkResult);
+                                        chrome.action.setBadgeText({ text: checkResult.count == 0 ? "" : checkResult.count.toString() });
+                                    });
                                 });
-                            });
-                        },
-                        err => {
-                            setLastCheck(new Date(0), () => {
-                                console.error(err);
-                                logger.log(err);
-                                chrome.action.setBadgeText({ text: "X" });
-                                chrome.alarms.create(
-                                    "sync-count",
-                                    {
-                                        delayInMinutes: 0.1,
-                                        periodInMinutes: PERIOD_IN_MINUTES
-                                    }
-                                );
-                            });
-                        }
-                    );
+                            },
+                            err => {
+                                chrome.action.setBadgeBackgroundColor({ color: "#808080" }, () => {
+                                    setLastCheck(0, () => {
+                                        console.error(err);
+                                        logger.log(err);
+                                        chrome.action.setBadgeText({ text: "X" });
+                                        chrome.alarms.create(
+                                            "sync-count",
+                                            {
+                                                delayInMinutes: 0.1,
+                                                periodInMinutes: PERIOD_IN_MINUTES
+                                            }
+                                        );
+                                    });
+                                });
+                            }
+                        );
+                    });
                 });
             } else {
                 chrome.alarms.create(
@@ -92,16 +109,13 @@ function initializeAlarm() {
 
 initializeAlarm();
 
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.local.get([CHECK_RESULT_KEY], ({ checkResult }) => {
-        let text = "?";
-        if (checkResult !== undefined) {
-            text = checkResult.count == 0 ? "" : checkResult.count.toString();
-        }
-        chrome.action.setBadgeText({ text });
-    })
-
-});
+chrome.storage.local.get([CHECK_RESULT_KEY], ({ checkResult }) => {
+    let text = "?";
+    if (checkResult !== undefined) {
+        text = checkResult.count == 0 ? "" : checkResult.count.toString();
+    }
+    chrome.action.setBadgeText({ text });
+})
 
 /**
  * https://developer.chrome.com/docs/extensions/mv3/migrating_to_service_workers/#event_listeners
