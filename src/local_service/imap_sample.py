77ee136788de854a8f4c386c492caf1a0892a7ae
Kaiwen Sun <skw_kevin@126.com>
2021-03-02 01:17:41 -0800

    fetch email details

--- src/local_service/imap_sample.py
+++ src/local_service/imap_sample.py
@@ -1,4 +1,5 @@
 from imaplib import IMAP4_SSL
+from datetime import datetime
 from config.settings import USER_NAME, IMAP_TOKEN, IMAP_ADDRESS, APP_NAME, APP_VERSION
 
 import email
@@ -10,29 +11,61 @@ def main():
         imap.noop()
         imap.xatom(f'ID ("name" "{APP_NAME}" "version" "{APP_VERSION}")')
         imap.login(USER_NAME, IMAP_TOKEN)
+        emails = []
         for folder in imap.list()[1]:
             if folder.startswith(b'() "/" "'):
-                print(folder)
-                folder = decode_folder_name(folder)[8:-1]
-                print(folder)
-                print(encode_folder_name(folder))
-                imap.select(f'"{encode_folder_name(folder)}"')
+                byte_folder_name = folder[8:-1]
+                print(byte_folder_name)
+                folder_name = decode_folder_name(byte_folder_name)
+                print(folder_name)
+                print(byte_folder_name.decode("ascii"))
+                res = imap.select(
+                    f'"{byte_folder_name.decode("ascii")}"', readonly=True)
+                print(res)
+                if (res[0] != 'OK'):
+                    raise Exception()
+                print("fetching unread emails")
+                mail_ids = imap.search(None, 'UnSeen')[1][0]
+                folder_emails = fetch_emails(imap, mail_ids)
+                for email in folder_emails:
+                    email["folder"] = folder_name
+                emails += folder_emails
                 print("====")
         print("done!")
-        imap.select(readonly=True)
-        _, mail_ids = imap.search(None, 'UnSeen')
-        mail_ids = mail_ids[0].split()
-        print(f"Total unread: {len(mail_ids)}")
+        print(f"Total unread: {len(emails)}")
+        print(emails)
 
-        for mail_id in mail_ids:
-            data = imap.fetch(mail_id, "(RFC822)")[1]
-            print("=" * 40)
-            print(mail_id)
-            for response_part in data:
-                if isinstance(response_part, tuple):
-                    msg = email.message_from_string(
-                        response_part[1].decode('utf-8'))
-                    print(parse_subject(msg))
+
+def fetch_emails(imap, mail_ids):
+    res = []
+    if isinstance(mail_ids, bytes):
+        mail_ids = mail_ids.decode('ascii')
+    if isinstance(mail_ids, str):
+        mail_ids = mail_ids.split()
+
+    for mail_id in mail_ids:
+        data = imap.fetch(mail_id, "(RFC822)")[1]
+        print("=" * 40)
+        print(mail_id)
+        for response_part in data:
+            if isinstance(response_part, tuple):
+                msg = email.message_from_string(
+                    response_part[1].decode('utf-8'))
+                subject = parse_header(msg["Subject"])
+                sender = email.utils.parseaddr(parse_header(msg["FROM"]))
+                receiver = email.utils.parseaddr(parse_header(msg["TO"]))
+                date = email.utils.parsedate_to_datetime(msg["Date"])
+                import pdb; pdb.set_trace()
+                timestamp = int(datetime.timestamp(date))
+                print(subject)
+                res.append({
+                    "eid": mail_id,
+                    "subject": subject,
+                    "sender": sender,
+                    "receiver": receiver,
+                    "timestamp": timestamp
+                })
+    return res
 
 
 def decode_folder_name(name):
@@ -40,14 +73,17 @@ def decode_folder_name(name):
 
 
 def encode_folder_name(name):
-    return codecs.encode(name, encoding='utf-7').replace(b"+", b"&")
+    # this method doesn't always work
+    return codecs.encode(name, encoding='utf-7').decode("ascii").replace("+", "&").replace("&-", "+-")
 
 
-def parse_subject(msg):
+def parse_header(line):
     res = []
-    for text, code in email.header.decode_header(msg["Subject"]):
+    for text, code in email.header.decode_header(line):
         if code is not None:
             text = text.decode(code)
+        elif isinstance(text, bytes):
+            text = text.decode("ascii")
         res.append(text)
     return "".join(res)
 
