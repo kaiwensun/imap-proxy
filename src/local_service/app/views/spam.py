149ec5db0c47d1c56f92545ee0b2efc76df0e512
Kaiwen Sun <skw_kevin@126.com>
2021-08-06 09:15:57 -0700

    detect spam emoji

--- src/local_service/app/views/spam.py
+++ src/local_service/app/views/spam.py
@@ -1,4 +1,5 @@
 import re
+import emoji
 
 
 def get_payload(msg):
@@ -17,10 +18,17 @@ def feature1(msg):
     """
     FROM_PATTERN = "^[a-z]+[0-9]+@gmail.com$"
     TO_PATTERN = "^undisclosed-recipients:;$"
-    SUBJECT_PATTERN = "^(💚|💎|😍)"
+    SUBJECT_PATTERN = ("^["
+                       u"\U0001F600-\U0001F64F"  # emoticons
+                       u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                       u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                       u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                       "]" )
     CONTENT_PATTERN = "http://.+\\.xyz"
     return bool(
         re.search(FROM_PATTERN, msg["from"]) and
         re.search(TO_PATTERN, msg["to"]) and
-        re.search(SUBJECT_PATTERN, msg["subject"]) and
+        len(msg["subject"]) > 3 and
+        msg["subject"][0] in emoji.UNICODE_EMOJI_ENGLISH and
+        msg["subject"][1] == ' ' and
         re.search(CONTENT_PATTERN, get_payload(msg)))
